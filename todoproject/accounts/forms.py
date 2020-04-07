ca0624311ed7eb6f8951a5dff07f1b6d5bfe87f4
SS1L <vlad7art13@gmail.com>
2020-04-07 12:24:45 +0300

    fix login bug

--- todoproject/accounts/forms.py
+++ todoproject/accounts/forms.py
@@ -2,11 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class LoginForm(forms.Form):
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
-
-
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
--- todoproject/accounts/views.py
+++ todoproject/accounts/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 
 from django.contrib.auth import authenticate, login, logout
 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .forms import CreateUserForm
 
 def signupPage(request):
@@ -24,20 +24,21 @@ def signupPage(request):
     return render(request, 'accounts/signup.html', context)
 
 def loginPage(request):
-    if request.method == "POST":
-        email = request.POST.get('email')
-        password = request.POST.get('password')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
 
-        user = authenticate(request, email=email, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Email or password is inncorect')
-
-    context = {}
-    return render(request, 'accounts/login.html', context)
+            if user is not None:
+                login(request, user)
+                return redirect('board')
+            else:
+                messages.error(request, 'Invalid login or password')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
 
 def logoutUser(request):
     logout(request)
